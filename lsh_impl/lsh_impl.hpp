9bc5d6c4470438aee0341f81b5c759e51bbb3a7f
Comzyh <comzyh@gmail.com>
2017-02-23 21:12:59 +0800

    finish search.

--- lsh_impl/lsh_impl.hpp
+++ lsh_impl/lsh_impl.hpp
@@ -7,8 +7,12 @@
 #include <unordered_set>
 #include <fstream>
 #include <functional>
+#include <utility>
+#include <algorithm>
+#include <queue>
 namespace lsh_impl {
 
+typedef int32_t hash_element_type; //
 
 //http://www.cnblogs.com/yeahgis/archive/2012/07/13/2590485.html
 //Generate std normal distribution
@@ -41,7 +45,7 @@ template<typename ElementType>
 struct Matrix {
     size_t row, col;
     ElementType *data;
-    Matrix(){}
+    Matrix() {}
     Matrix(size_t row, size_t col): row(row), col(col) {
         data = new ElementType[col * row];
     }
@@ -81,11 +85,11 @@ struct Vector
 {
     size_t d; //dimension
     ElementType *data;
-    Vector(){}
+    Vector() {}
     Vector(size_t d): d(d) {
         data = new ElementType[d];
     }
-    Vector(size_t d, ElementType *data): d(d), data(data){}
+    Vector(size_t d, ElementType *data): d(d), data(data) {}
 
     const ElementType &operator[](const size_t index) const {
         return data[index];
@@ -105,6 +109,13 @@ struct Vector
     float operator*(const Vector<ElementType> &v) const {
         return (*this) * v.data;
     }
+    float square_dist(const ElementType *v) {
+        float result = 0;
+        for (size_t i = 0; i < d; i ++) {
+            result += (data[i] - v[i]) * (data[i] - v[i]);
+        }
+        return result;
+    }
 };
 
 struct LshIndexParams
@@ -114,20 +125,18 @@ struct LshIndexParams
     float W; // W
     size_t probe_num; // number proble to use
     LshIndexParams(size_t table_num, size_t function_num, float W, size_t probe_num):
-    table_num(table_num), function_num(function_num), W(W), probe_num(probe_num) {}
+        table_num(table_num), function_num(function_num), W(W), probe_num(probe_num) {}
 };
 
 template<typename ElementType>
 struct LSH_Table
 {
-    typedef int32_t hash_element_type; //
-    typedef hash_element_type* hash_type; // 
-    typedef std::unordered_multimap<
-        hash_type, // Key
-        size_t, // Value
-        std::function<size_t(hash_type)>, // hasher
-        std::function<bool(hash_type, hash_type)> //key_equal
-        > Table_T;
+    typedef hash_element_type* hash_type; //
+    typedef std::unordered_multimap <hash_type, // Key
+            size_t, // Value
+            std::function<size_t(hash_type)>, // hasher
+            std::function<bool(hash_type, hash_type)> //key_equal
+            > Table_T;
     Matrix<ElementType> a;
     Vector<ElementType> b;
     Matrix<hash_element_type> hashs;
@@ -136,26 +145,26 @@ struct LSH_Table
     float W;
     Table_T table;
     LSH_Table(size_t feature_size, size_t function_num, float W, size_t row):
-    feature_size(feature_size), function_num(function_num), W(W),
-    a(function_num, feature_size),b(function_num), hashs(row, function_num) {
+        feature_size(feature_size), function_num(function_num), W(W),
+        a(function_num, feature_size), b(function_num), hashs(row, function_num) {
         table = Table_T(
-            row, //
-            [function_num](hash_type hash)-> size_t { // Hasher
-                size_t result = 0;
-                for (size_t i = 0; i < function_num; i++) {
-                    result = result * 47 + *reinterpret_cast<uint32_t*>(hash + i);
-                }
-                return result;
-            },
-            [function_num](hash_type u, hash_type v)-> bool {
-                for (size_t i = 0; i < function_num; i++) {
-                    if (u[i] != v[i]) {
-                        return false;
-                    }
-                }
-                return true;
+                    row, //
+        [function_num](hash_type hash)-> size_t { // Hasher
+            size_t result = 0;
+            for (size_t i = 0; i < function_num; i++) {
+                result = result * 47 + *reinterpret_cast<uint32_t*>(hash + i);
             }
-        );
+            return result;
+        },
+        [function_num](hash_type u, hash_type v)-> bool {
+            for (size_t i = 0; i < function_num; i++) {
+                if (u[i] != v[i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+                );
         for (size_t i = 0; i < function_num; i ++) {
             for (size_t j = 0; j < feature_size; j ++) {
                 a[i][j] = gaussrand();
@@ -186,9 +195,10 @@ struct LSH_Table
             Vector<hash_element_type> hash(function_num, const_cast<hash_type>(hashs[i]));
             Vector<ElementType> v(feature_size, const_cast<ElementType*>(data[i]));
             getKey(v, hash.data);
-            table.insert(std::make_pair(hash.data, i));
+            // table.insert(std::make_pair(hash.data, i));
         }
     }
+
     ~LSH_Table() {
         delete[] a.data;
         delete[] b.data;
@@ -201,22 +211,60 @@ class LSH_Index
 {
 public:
     LSH_Index(const Matrix<ElementType> &data, const LshIndexParams &params): data(data), params(params) {
-        for (size_t i = 0; i < params.table_num; i++) { 
+        feature_size = data.col;
+        for (size_t i = 0; i < params.table_num; i++) {
             //size_t feature_size, size_t function_num, float W, size_t rows
-            tables.push_back(LSH_Table<ElementType> (data.col, params.function_num, params.W, data.row));
+            tables.push_back(LSH_Table<ElementType> (feature_size, params.function_num, params.W, data.row));
         }
     }
+    ~LSH_Index() {
+
+    };
     void buildIndex() {
+        #pragma omp parallel for
         for (size_t i = 0; i < params.table_num; i ++) {
             tables[i].add(data);
             printf("table %4lu initialized\n", i);
         }
-
     }
-    ~LSH_Index(){
 
-    };
+    void knnSearch(Matrix<ElementType> &queries, Matrix<int> &indices, Matrix<float> &dists, int nn, const LshIndexParams &params) {
+        #pragma omp parallel for
+        for (size_t i = 0; i < queries.row; i++) {
+            findNeighbors(queries[i], indices[i], dists[i], nn);
+            printf("Found\n");
+        }
+    }
+    void findNeighbors(ElementType *query, int* indices, float *dists, int nn) {
+        Vector<ElementType> q(feature_size, query); // query vector
+
+        std::unordered_set<int> in;
+        std::priority_queue < std::pair<float, int>/* dist, indexs*/ > result;
+        Vector<hash_element_type > hash(params.function_num);
+
+        for (size_t i = 0; i < tables.size(); i++) {
+            tables[i].getKey(Vector<ElementType>(feature_size, query), hash.data);
+            auto range = tables[i].table.equal_range(hash.data);
+            for (auto it = range.first; it != range.second; it ++) {
+                if (in.count(it->second)) {
+                    continue;
+                }
+                result.push(std::make_pair(q.square_dist(query), it->second));
+                if (result.size() > nn) {
+                    result.pop();
+                }
+            }
+        }
+        for (size_t i = result.size() - 1; !result.empty(); i++) {
+            indices[i] = result.top().second;
+            dists[i] = result.top().first;
+            result.pop();
+        }
+        delete[] hash.data;
+    }
+
 private:
+    size_t feature_size;
     Matrix<ElementType> data;
     LshIndexParams params;
     std::vector<LSH_Table<ElementType> > tables;
--- test.cpp
+++ test.cpp
@@ -8,37 +8,55 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-	// CommandLine parser
-	cmdline::parser parser;
-	parser.add<string>("base", 'b', "Base data path.", true);
-	parser.add<string>("query", 'q', "Query data path.", true);
-	parser.add<string>("ground", 'g', "Ground_Truth data path.", true);
-	parser.add<int>("table_num", 't', "Number of table", false, 10);
-	parser.add<int>("function_num", 'f', "Number of function", false, 10);
-	parser.add<float>("W", 'w', "W", false, 10.0f);
-	parser.add<int>("probe", 'p', "number of probe", false, 200);
-	parser.parse_check(argc, argv);
+    // CommandLine parser
+    cmdline::parser parser;
+    parser.add<string>("base", 'b', "Base data path.", true);
+    parser.add<string>("query", 'q', "Query data path.", true);
+    parser.add<string>("ground", 'g', "Ground_Truth data path.", true);
+    parser.add<int>("table_num", 't', "Number of table", false, 10);
+    parser.add<int>("function_num", 'f', "Number of function", false, 1);
+    parser.add<float>("W", 'w', "W", false, 10.0f);
+    parser.add<int>("probe", 'p', "number of probe", false, 200);
+    parser.parse_check(argc, argv);
 
-	//LoadData
-	//http://corpus-texmex.irisa.fr/
-	size_t filesize, dimension;
+    //LoadData
+    //http://corpus-texmex.irisa.fr/
+    size_t filesize, dimension;
 
-	lsh_impl::Matrix<float> base = lsh_impl::Matrix<float>::load_from_file(parser.get<string>("base").c_str());
-	lsh_impl::Matrix<float> query = lsh_impl::Matrix<float>::load_from_file(parser.get<string>("query").c_str());
-	lsh_impl::Matrix<int> ground_truth = lsh_impl::Matrix<int>::load_from_file(parser.get<string>("ground").c_str());
-	printf("Base        : %8lu row, %4lu col\n", base.row, base.col);
-	printf("Query       : %8lu row, %4lu col\n", query.row, query.col);
-	printf("Ground truth: %8lu row, %4lu col\n", ground_truth.row, ground_truth.col);
+    lsh_impl::Matrix<float> base = lsh_impl::Matrix<float>::load_from_file(parser.get<string>("base").c_str());
+    lsh_impl::Matrix<float> query = lsh_impl::Matrix<float>::load_from_file(parser.get<string>("query").c_str());
+    lsh_impl::Matrix<int> ground_truth = lsh_impl::Matrix<int>::load_from_file(parser.get<string>("ground").c_str());
+    printf("Base        : %8lu row, %4lu col\n", base.row, base.col);
+    printf("Query       : %8lu row, %4lu col\n", query.row, query.col);
+    printf("Ground truth: %8lu row, %4lu col\n", ground_truth.row, ground_truth.col);
 
-	//LshIndexParams
-	//size_t table_num, size_t function_num, float W, size_t probe_num
-	lsh_impl::LSH_Index<float> index(base, lsh_impl::LshIndexParams(
-	                                     parser.get<int>("table_num"),
-	                                     parser.get<int>("function_num"),
-	                                     parser.get<float>("W"),
-	                                     parser.get<int>("probe")
-	                                 ));
-	index.buildIndex();
+    //LshIndexParams
+    //size_t table_num, size_t function_num, float W, size_t probe_num
+    lsh_impl::LshIndexParams index_params(
+        parser.get<int>("table_num"),
+        parser.get<int>("function_num"),
+        parser.get<float>("W"),
+        parser.get<int>("probe")
+    );
+    lsh_impl::LSH_Index<float> index(base, index_params);
+    index.buildIndex();
 
-	return 0;
+    //find
+    int nn = ground_truth.col;
+    lsh_impl::Matrix<int> indices(query.row, nn);
+    lsh_impl::Matrix<float> dists(query.row, nn);
+    index.knnSearch(query, indices, dists, nn, index_params);
+
+    for (size_t i = 0; i < 1; i++) {
+        printf("query %4d:\n", i);
+        for (size_t j = 0; j < nn; j++) {
+            printf("%6d ", indices[i][j]);
+        }
+        printf("\n");
+        for (size_t j = 0; j < nn; j++) {
+            printf("%6d ", ground_truth[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
 }

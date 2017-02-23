d0e378648e46ac68a5f2992df4f620c390c1a209
Comzyh <comzyh@gmail.com>
2017-02-24 01:00:04 +0800

    query_limit

--- test.cpp
+++ test.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <string>
 #include <fstream>
+#include <unordered_set>
 #include "vendor/cmdline/cmdline.h"  // argparser
 #include "lsh_impl/lsh_impl.hpp"
 
@@ -17,6 +18,7 @@ int main(int argc, char **argv)
     parser.add<int>("function_num", 'f', "Number of function", false, 1);
     parser.add<float>("W", 'w', "W", false, 10.0f);
     parser.add<int>("probe", 'p', "number of probe", false, 200);
+    parser.add<int>("limit", 'l', "limit the query number", false, -1);
     parser.parse_check(argc, argv);
 
     //LoadData
@@ -26,6 +28,7 @@ int main(int argc, char **argv)
     lsh_impl::Matrix<float> base = lsh_impl::Matrix<float>::load_from_file(parser.get<string>("base").c_str());
     lsh_impl::Matrix<float> query = lsh_impl::Matrix<float>::load_from_file(parser.get<string>("query").c_str());
     lsh_impl::Matrix<int> ground_truth = lsh_impl::Matrix<int>::load_from_file(parser.get<string>("ground").c_str());
+
     printf("Base        : %8lu row, %4lu col\n", base.row, base.col);
     printf("Query       : %8lu row, %4lu col\n", query.row, query.col);
     printf("Ground truth: %8lu row, %4lu col\n", ground_truth.row, ground_truth.col);
@@ -45,18 +48,40 @@ int main(int argc, char **argv)
     int nn = ground_truth.col;
     lsh_impl::Matrix<int> indices(query.row, nn);
     lsh_impl::Matrix<float> dists(query.row, nn);
+
+    // lsh_impl::Matrix<float> fake_query;
+    // fake_query.row = 100;
+    // fake_query.col = base.col;
+    // fake_query.data = base.data;
+
+
+    //limit the count of query for debug
+    int query_limit = parser.get<int>("limit");
+    if (query_limit != -1) {
+        query.row = query_limit;
+    }
+    
     index.knnSearch(query, indices, dists, nn, index_params);
 
-    for (size_t i = 0; i < 1; i++) {
-        printf("query %4d:\n", i);
+    double recall = 0;
+    for (size_t i = 0; i < query.row; i++) {
+        // printf("query %4d:\n", i);
+        lsh_impl::Vector<float> q(query.col, query[i]);
+        unordered_set<int> truth;
+        //GroundTruth
         for (size_t j = 0; j < nn; j++) {
-            printf("%6d ", indices[i][j]);
+            // printf("(%d, %.0f) ", ground_truth[i][j], q.square_dist(base[ground_truth[i][j]]));
+            truth.insert(ground_truth[i][j]);
         }
-        printf("\n");
+        // printf("\n\n");
+        //Mine
         for (size_t j = 0; j < nn; j++) {
-            printf("%6d ", ground_truth[i][j]);
+            // printf("(%d, %.0f) ", indices[i][j], dists[i][j]);
+            recall += truth.count(indices[i][j]);
         }
-        printf("\n");
+        printf("\n"); 
     }
+    recall /= query.row * nn;
+    printf("Recall: %lf\n", recall);
     return 0;
 }
